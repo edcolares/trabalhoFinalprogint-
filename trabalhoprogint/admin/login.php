d01f2f7fde7a6351079a401fa05ca17c3893dc84
EstefaniEvangelista <estefanievangelista96@gmail.com>
2021-07-09 19:54:04 -0300

    #front-end - ajuste no menu e login.

--- trabalhoprogint/admin/login.php
+++ trabalhoprogint/admin/login.php
@@ -22,7 +22,7 @@ logar($conexao);
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 	<title>Painel de Acesso - Área Administrativa</title>
 
@@ -30,22 +30,18 @@ logar($conexao);
 
 <body>
 
-	<div class="form-group container-fluid">
-
-		<form class="form-signin" action="" method="post">
-			<h1 class="h3 mb-3 font-weight-normal">Painel de Acesso</h1>
-			<label for="inputEmail" class="sr-only">Email</label>
-			<input type="email" id="email" name="email" class="form-control" placeholder="Email" required="" autofocus="">
-
-			<label for="inputPassword" class="sr-only">Senha</label>
-			<input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" required="">
-
+	<div class="container">
+		<form class="form-login" action="" method="post">
+			<h1 class="login">Painel de Acesso</h1>
+			<input type="email" id="email" name="email" class="form-email" placeholder="Email*" required="" autofocus="">
+			<input type="password" id="senha" name="senha" class="form-senha" placeholder="Senha*" required="">
+			<br>
 			<?php
 			logar($conexao);
 			?>
-
-			<button class="btn btn-lg btn-primary btn-block" type="submit" name='logar'>Logar</button>
+			<button class="btn_logar" type="submit" name='logar' data-toggle="tooltip" title="Logar">Logar</button>
 		</form>
+		
 
 	</div>
 
--- trabalhoprogint/admin/template/menu.php
+++ trabalhoprogint/admin/template/menu.php
@@ -1,8 +1,71 @@
-<aside>
-		<ul>
-			<li><a href="index.php">Painel</a></li>
-			<li><a href="paginas.php">Páginas</a></li>
-			<li><a href="usuarios.php">Usuários</a></li>
-			<li><a href="sair.php">Sair</a></li>
-		</ul>
-</aside>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv=”content-type” content="text/html;" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="keywords" content="tags" />
+    <title>Sistema Administrativo</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="admin/css/style.css" />
+        
+    <!-- ESTILOS PARA ESTA PÁGINA -->
+    <!-- Nesse caso, este estilo é apenas para inserir imagens -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
+    <!-- JAVASCRIPT E JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+	<style>
+		*{
+			padding: 0px;
+			margin: 0px;
+			box-sizing: border-box;
+		}
+		.rodape_final{
+			background-color: #000;
+			color:#fff;
+		}
+	</style>
+
+</head>
+
+<body>
+<!-- INÍCIO DO MENU SUPERIOR -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#links-menu" style="background-color: white;">
+					<i class="material-icons">menu</i>
+				</button>
+			</div>
+			
+
+			<nav id="links-menu" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Administrativo</a></li>
+					<li><a href="#">Configuração</a></li>
+					<li><a href="#">Usuários</a></li>
+					<li><a href="#">Notícias</a></li>
+				</ul>
+			</nav>
+		</div>
+	</nav>
+	<p>teste de mensagem</p>
+<!-- FIM DO MENU SUPERIOR -->
+
+	<article>
+		<p>corpo da pagina</p>
+	</article>
+	<footer class="rodape_final">
+		<p>Aqui vai ser o rodapé</p>
+	</footer>
+</body>
+</html>
